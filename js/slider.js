f5c800a864bb44bbef0335f2af5c152b8fbe02cb
Michał <kwestorowski.m@gmail.com>
2017-09-02 23:15:03 +0200

    Making slider for partners

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,12 @@
-/**
- * Created by MKwestorowski on 04.05.2017.
- */
+$('.slides').slick({
+
+    dots: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
 
 
-let imagesArray = {
-    first: 'images/securitybackground.jpg',
-    second: 'images/cds.jpg',
-    third: 'images/openshop-pano.jpg',
-    fourth: 'images/smartphone.jpg'
-}
-
-
-// function intervalForSlider() {
-//
-//     let slider = setInterval(function () {
-//         $('.imageZajawka').attr('src', imagesArray.first);
-//     }, 10000)
-// }
-//
-// intervalForSlider();
-
-timerForSlider();
-
-function timerForSlider() {
-    let countSecTimer = 30;
-    let intervalForSlider = setInterval(function () {
-            if (countSecTimer === 24) {
-                $('.imageZajawka').fadeOut('fast', function() {$('.imageZajawka').attr('src', imagesArray.first); $('.imageZajawka').fadeIn('fast');
-                countSecTimer--
-            })}
-            else if (countSecTimer === 16) {
-                $('.imageZajawka').fadeOut('fast', function() {$('.imageZajawka').attr('src', imagesArray.second); $('.imageZajawka').fadeIn('fast');
-                    countSecTimer--
-                })}
-            else if (countSecTimer === 5) {
-                $('.imageZajawka').fadeOut('fast', function() {$('.imageZajawka').attr('src', imagesArray.third); $('.imageZajawka').fadeIn('fast');
-                    countSecTimer--
-                })}
-            else if (countSecTimer === 0
-            ) {
-                $('.imageZajawka').fadeOut('fast', function() {$('.imageZajawka').attr('src', imagesArray.fourth); $('.imageZajawka').fadeIn('fast');
-                clearInterval(intervalForSlider);
-                timerForSlider();
-            })}
-            else {
-                countSecTimer--
-            }
-
-        }
-        , 1000);
-}
+});
