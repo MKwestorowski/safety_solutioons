f7523f556cfc30f24c6a05d3ff08412c7a802dc8
Michał <kwestorowski.m@gmail.com>
2017-09-03 19:31:10 +0200

    Made video background

--- js/video.js
+++ js/video.js
@@ -1,32 +1,38 @@
-//jQuery is required to run this code
-$(document).ready(function () {
+/** Document Ready Functions **/
+/********************************************************************/
 
+$( document ).ready(function() {
+
+    // Resive video
     scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container ');
+    initBannerVideoSize('.video-container .filter');
     initBannerVideoSize('.video-container video');
 
-    $(window).on('resize', function () {
+    $(window).on('resize', function() {
         scaleVideoContainer();
         scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container');
+        scaleBannerVideoSize('.video-container .filter');
         scaleBannerVideoSize('.video-container video');
     });
 
 });
 
+/** Reusable Functions **/
+/********************************************************************/
+
 function scaleVideoContainer() {
 
     var height = $(window).height();
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height', unitHeight);
+    $('.homepage-hero-module').css('height',unitHeight);
 
 }
 
-function initBannerVideoSize(element) {
+function initBannerVideoSize(element){
 
-    $(element).each(function () {
+    $(element).each(function(){
         $(this).data('height', $(this).height());
         $(this).data('width', $(this).width());
     });
@@ -35,28 +41,33 @@ function initBannerVideoSize(element) {
 
 }
 
-function scaleBannerVideoSize(element) {
+function scaleBannerVideoSize(element){
 
     var windowWidth = $(window).width(),
-        windowHeight = $(window).height() + 5,
+        windowHeight = $(window).height(),
         videoWidth,
         videoHeight;
 
+    console.log(windowHeight);
 
-    $(element).each(function () {
-        var videoAspectRatio = $(this).data('height') / $(this).data('width');
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width'),
+            windowAspectRatio = windowHeight/windowWidth;
 
-        $(this).width(windowWidth);
-
-        if (windowWidth < 1000) {
+        if (videoAspectRatio > windowAspectRatio) {
+            videoWidth = windowWidth;
+            videoHeight = videoWidth * videoAspectRatio;
+            $(this).css({'top' : -(videoHeight - windowHeight) / 2 + 'px', 'margin-left' : 0});
+        } else {
             videoHeight = windowHeight;
             videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px'});
-
-            $(this).width(videoWidth).height(videoHeight);
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
         }
 
+        $(this).width(videoWidth).height(videoHeight);
+
         $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
 
     });
 }
