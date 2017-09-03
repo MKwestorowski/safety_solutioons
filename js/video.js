6d382b804f3fb9ccf0259ab83d8ad21588ecd936
Michał <kwestorowski.m@gmail.com>
2017-09-03 14:46:47 +0200

    making responsive background

--- js/video.js
+++ js/video.js
@@ -1,16 +1,16 @@
 //jQuery is required to run this code
-$( document ).ready(function() {
+$(document).ready(function () {
 
     scaleVideoContainer();
 
     initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container ');
     initBannerVideoSize('.video-container video');
 
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
         scaleVideoContainer();
         scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container');
         scaleBannerVideoSize('.video-container video');
     });
 
@@ -18,15 +18,15 @@ $( document ).ready(function() {
 
 function scaleVideoContainer() {
 
-    var height = $(window).height() + 5;
+    var height = $(window).height();
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+    $('.homepage-hero-module').css('height', unitHeight);
 
 }
 
-function initBannerVideoSize(element){
+function initBannerVideoSize(element) {
 
-    $(element).each(function(){
+    $(element).each(function () {
         $(this).data('height', $(this).height());
         $(this).data('width', $(this).width());
     });
@@ -35,24 +35,23 @@ function initBannerVideoSize(element){
 
 }
 
-function scaleBannerVideoSize(element){
+function scaleBannerVideoSize(element) {
 
     var windowWidth = $(window).width(),
         windowHeight = $(window).height() + 5,
         videoWidth,
         videoHeight;
 
-    console.log(windowWidth);
 
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+    $(element).each(function () {
+        var videoAspectRatio = $(this).data('height') / $(this).data('width');
 
         $(this).width(windowWidth);
 
-        if(windowWidth < 1000){
+        if (windowWidth < 1000) {
             videoHeight = windowHeight;
             videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+            $(this).css({'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px'});
 
             $(this).width(videoWidth).height(videoHeight);
         }
